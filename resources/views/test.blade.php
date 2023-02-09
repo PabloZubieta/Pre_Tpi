c7693a990e274b18fb9dbd54687b7c53b1f5ddd3
PabloZubieta <Pablo-Fernando.ZUBIETA-RODRIGUEZ@cpnv.ch>
2023-02-09 14:42:24 +0100

    git flow

---
+++ resources/views/test.blade.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @file test.blade.php
+ * @brief file description
+ * @author Created by Pablo-Fernando.ZUBIE
+ * @version 07.02.2023
+ */
+?>
+<h2>hello my bro</h2>
+@for($i=0; $i<= $id; $i++)
+    Hello {$i}
+@endfor
+
+
+
