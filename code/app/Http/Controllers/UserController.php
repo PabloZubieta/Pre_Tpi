3ac41ee0e937d71b87adbcd2bf51cef56ab22dfc
PabloZubieta <Pablo-Fernando.ZUBIETA-RODRIGUEZ@cpnv.ch>
2023-02-27 11:49:39 +0100

    model begin understanding

--- code/app/Http/Controllers/UserController.php
+++ code/app/Http/Controllers/UserController.php
@@ -3,32 +3,51 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     //
-    public function signin(){
+    public function signin()
+    {
         return view('users.signin');
     }
-    public  function login(){
+    public  function login()
+    {
         return view('users.login');
     }
-    public function activate(Request $request){
+    public function activate(Request $request)
+    {
         $formFields = $request ->validate([
             'email'=>['required','email'],
             'car_seat'=>['required'],
             'password'=>'required'|'confirmed'|'min:8'
         ]);
-        $formFields['password'] = bcrypt($formFields['password']);
+
+        // acronyme exist?
+        $user = User::firstWhere('acronym', $request->username);
+        if ($user) {
+            $user->active = true;
+            $user->password = bcrypt($formFields['password']);
+            $user->save();
+
+        } else {
+            return back()->withErrors(['email'=>'erreur de dans l\'un des champs'])->onlyInput('email');
+        }
+
+
+
     }
-    public function logout(Request $request){
+    public function logout(Request $request)
+    {
         auth()->logout();
 
         $request->session()->invalidate();
         $request->session()->regenerateToken();
     }
-    public function log(Request $request){
+    public function log(Request $request)
+    {
         $formFields = $request ->validate([
             'username'=>['required'],
             'password'=>'required'
