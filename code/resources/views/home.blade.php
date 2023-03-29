c36bee070b050baa64d0f9b1fd1057470aa948db
PabloZubieta <Pablo-Fernando.ZUBIETA-RODRIGUEZ@cpnv.ch>
2023-03-29 07:48:11 +0200

    Pres last commit

--- code/resources/views/home.blade.php
+++ code/resources/views/home.blade.php
@@ -12,14 +12,13 @@ $title ="Ecolopnv";
 @section('content')
     <div style="padding:5px;">
         <div class="container col-lg-6 col-md-8 col-sm-12 " style="background-color: #386641; border-radius: 10px; color: #F2EBCF;padding-top: 20px;padding-bottom: 30px">
-            <h2 style=" text-align: center;padding-bottom: 15px">Bienvenu Ecolopnv</h2>
+            <h2 style=" text-align: center;padding-bottom: 15px">Bienvenu sur Ecolopnv</h2>
             <div class="row" >
                 <div class="col-lg-6 col-md-12 col-sm-12"><p>
-                        Vous voyez le réchauffement climatique s'intensifier, Les gouvernements prendre des décisions irresponsables, La Sixième extinction de masse arriver, des guerres éclater, la démocratie reculer, la centralité et la fragilité des chaines d'approvisionnement.
-                    </p></div>
+                        Vous voyez le réchauffement climatique s'intensifier, les gouvernements prendre des décisions irresponsables, la sixième extinction de masse arriver, des guerres éclater, la démocratie reculer, des chaines d'approvisionnement être mises en péril?</p></div>
                 <div class="col-lg-6 col-md-12 col-sm-12" ><p>
-                        Ne dites pas un mot de plus Ecolopnv est là pour vous. Certes il ne permet en aucun point de contrevenir de manière significative aux problèmes cité ci-dessus mais il vous donnera bonne conscience car vous au moins vous faites du covoiturage. Vous pourrez ainsi continuer de moraliser les autres conducteurs et vous prélasser dans la plus grande auto satisfaction
-                    </p></div>
+                        Ne dites pas un mot de plus! Ecolopnv est là pour vous.<br>
+                        Certes, ce site[?] ne vous permettra pas d'avoir une influence significative sur tous ces problèmes... mais il aura le mérite de vous donner bonne conscience. Hé oui, vous, au moins, vous ferez du covoiturage. Vous pourrez ainsi continuer de moraliser les autres conducteurs, et vous prélasser dans la plus grande auto-satisfaction.</p></div>
             </div>
 
 
